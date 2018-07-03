41001bd86a73d9acbcb70a15698862b2a7de3756
Niss95 <dsellemann@gmail.com>
2018-07-03 11:42:02 +0200

    stable version after gradle update

--- app/src/main/java/de/seraphimgroup/inventarmanager/MainActivity.kt
+++ app/src/main/java/de/seraphimgroup/inventarmanager/MainActivity.kt
@@ -1,12 +1,32 @@
 package de.seraphimgroup.inventarmanager
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.AppCompatActivity
+import de.seraphimgroup.inventarmanager.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mDrawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mDrawerLayout = findViewById(R.id.drawer_layout)
+
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            // set item as selected to persist highlight
+            menuItem.isChecked = true
+            // close drawer when item is tapped
+            mDrawerLayout.closeDrawers()
+
+            // Add code here to update the UI based on the item selected
+            // For example, swap UI fragments here
+
+            true
+        }
     }
 }
